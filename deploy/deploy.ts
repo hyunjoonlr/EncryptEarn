e9751000055c9d78cf1327ec4eb2243aa0989c24
hyunjoonlr <axpeokn410@outlook.com>
2025-11-01 10:28:41 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,25 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedToken = await deploy("ERC7984USDT", {
     from: deployer,
     log: true,
+    args: [],
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const EncryptEarn = await deploy("EncryptEarn", {
+    from: deployer,
+    log: true,
+    args: [deployedToken.address],
+  });
+
+  const { execute } = hre.deployments;
+
+  await execute("ERC7984USDT", { from: deployer, log: true }, "setMinter", EncryptEarn.address);
+
+  console.log(`ERC7984USDT contract: ${deployedToken.address}`);
+  console.log(`EncryptEarn contract: ${EncryptEarn.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_confidentialEncryptEarn"; // id required to prevent reexecution
+func.tags = ["EncryptEarn"];
